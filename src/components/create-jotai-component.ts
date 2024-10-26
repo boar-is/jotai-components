cafe4c32436e7e67889c0f2751f61d33b3db0dac
Boaris <boriszubc@gmail.com>
2024-10-26 17:00:33 +0300

    Fix forwardRef's element type

--- src/components/create-jotai-component.ts
+++ src/components/create-jotai-component.ts
@@ -1,6 +1,7 @@
 import { type Atom, atom, useAtomValue } from 'jotai/index'
 import {
   type ComponentPropsWithoutRef,
+  type ElementRef,
   type ElementType,
   createElement,
   forwardRef,
@@ -16,33 +17,35 @@ type JotaiProps<E extends ElementType, P = ComponentPropsWithoutRef<E>> = {
 }
 
 export const createJotaiComponent = <E extends ElementType>(elementType: E) => {
-  const JotaiComponent = forwardRef<E, JotaiProps<E>>((props, ref) => {
-    const [atomPropsEntries, basicPropsEntries] = separate(
-      Object.entries(props),
-      (it) => it[0].startsWith('$'),
-    )
+  const JotaiComponent = forwardRef<ElementRef<E>, JotaiProps<E>>(
+    (props, ref) => {
+      const [atomPropsEntries, basicPropsEntries] = separate(
+        Object.entries(props),
+        (it) => it[0].startsWith('$'),
+      )
 
-    const propsAtom = useConstant(() =>
-      atom((get) =>
-        Object.fromEntries(
-          atomPropsEntries.map((it) => [it[0].replace('$', ''), get(it[1])]),
+      const propsAtom = useConstant(() =>
+        atom((get) =>
+          Object.fromEntries(
+            atomPropsEntries.map((it) => [it[0].replace('$', ''), get(it[1])]),
+          ),
         ),
-      ),
-    )
+      )
 
-    const atomProps = useAtomValue(propsAtom)
-    const { children, ...basicProps } = Object.fromEntries(basicPropsEntries)
+      const atomProps = useAtomValue(propsAtom)
+      const { children, ...basicProps } = Object.fromEntries(basicPropsEntries)
 
-    return createElement(
-      elementType,
-      {
-        ref,
-        ...basicProps,
-        ...atomProps,
-      },
-      children,
-    )
-  })
+      return createElement(
+        elementType,
+        {
+          ref,
+          ...basicProps,
+          ...atomProps,
+        },
+        children,
+      )
+    },
+  )
 
   JotaiComponent.displayName = getDisplayName(elementType)
 
